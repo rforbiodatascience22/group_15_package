476576f8b25ab2b143e5831a1a7270d60dbee55d
helenewegener1 <s165827@student.dtu.dk>
2022-03-21 08:59:17 +0000

    Helene was the person

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Title: transcription_table
+#'
+#' Description: Transcripts RNA to amino acid.
+#'
+#' @format
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"transcription_table"
